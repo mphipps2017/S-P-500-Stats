b6824a43c8df9e067154eed84c3a9197166c285b
Matthew <mphipps@iastate.edu>
2020-11-23 22:42:01 -0600

    Created main.py and added some comments to it

---
+++ main.py
@@ -0,0 +1,4 @@
+
+# Calculate the 1,2,3,5,8,10,15,20, 25, 30 year average rolling averages
+# Calculate the variance for all the above
+# Use these values to calculate the expected return for all the above
